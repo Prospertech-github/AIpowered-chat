96ca04a7ae98e55b50520e5c93c34d374172b316
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-02-19 03:47:47 +0100

    feat: initial project setup (Colors, CSS reset and logo)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World. girls on the block</h1>
+      <p>Whereas disregard and contempt for human rights have resulted</p>
+    </>
+  )
+}
+
+export default App
